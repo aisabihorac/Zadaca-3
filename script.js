6b97064d5b899710b790cbcf8bb7221e3d04ec11
aisa <aisabihorac965@gmail.com>
2024-08-12 22:39:20 +0200

    Zadatak 3

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+//Prvi zadatak
 for(let i=1;i<=10;i++){
     if(i%2==0){
         for(j=1;j<=10;j++){
             if(j%2==0){
-                console.log(i*j)
+                console.log(i*j);
             }
         }
     }
 }
+//Drugi zadatak
+let jezici  = ["JavaScript", "Python", "Java", "C++"];
+jezici.forEach(jezik=>console.log(jezik));
